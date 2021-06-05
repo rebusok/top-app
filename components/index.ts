33680a5a7fc2e07612165ff07f5e890a196b0320
Yuriy <rebusok@yandex.ru>
2021-06-05 16:24:59 +0300

    add 2 components

---
+++ components/index.ts
@@ -0,0 +1,6 @@
+import {Htag} from "./Htag/Htag";
+import {Button} from "./Button/Button";
+export {
+    Htag,
+    Button
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css';
 import {AppProps} from "next/app";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }:AppProps):JSX.Element {
-  return <Component {...pageProps} />;
+function MyApp({Component, pageProps}: AppProps): JSX.Element {
+    return (
+        <>
+            <Head>
+                <title>MyTop</title>
+                <link key={1} rel="icon" href="/favicon.ico"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
+                          rel="stylesheet"/>
+            </Head>
+            <Component {...pageProps} />;
+        </>
+);
 }
 
 export default MyApp;
